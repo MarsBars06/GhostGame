e06225aa2e7d28764adb9e951c79bd392fbf7001
MarsBars06 <68951027+MarsBars06@users.noreply.github.com>
2021-04-08 17:39:14 +0100

    Update GhostGame.py
    Added a verification check to make sure playsound is installed and also added pauses in between the console printing output.

--- console/GhostGame.py
+++ console/GhostGame.py
@@ -1,12 +1,23 @@
 # Made by MarsBars06 on Github
 from random import randint
 # Importing the randint function which chooses a random integer
-import playsound
-# Imports the playsound library, better OS compatibility than winsound
+from time import sleep
+# Imports the sleep function from the time library.
 import os
-SFX_enabled = True
-# If you want to disable the sound effects played, change this line to:
-# SFX_enabled = False
+try:
+    SFX_enabled = True
+    # If you want to disable the sound effects played, change this line to:
+    # SFX_enabled = False
+    import playsound
+    # Imports the playsound library, better OS compatibility than winsound
+except:
+    print("The module 'playsound' is not installed")
+    sleep(0.5)
+    print("Please run the command 'pip install playsound'")
+    sleep(0.5)
+    SFX_enabled = False
+    print("SFX will not be played")
+    sleep(0.5)
 if SFX_enabled:
     dirname = os.path.dirname(__file__)
     LoseSound_filename = os.path.join(dirname, 'resources/LoseSound.wav')
@@ -14,7 +25,8 @@ if SFX_enabled:
     # These lines of code put the relative path of the SFX wav files into variables
     # which will be used later on.  This only runs if the SFX_enabled variable is True.
 # These are based on relative paths.
-print("Ghost Game")
+print("Ghost Game - Made by MarsBars06 at Github.")
+sleep(0.5)
 feelingBrave = True
 # The feelingBrave variable will come in use when it is used in a while loop
 score = 0
@@ -23,7 +35,9 @@ while feelingBrave:
     ghostDoor = randint(1,3)
     # The ghost door is a random door that, when chosen by the user, ends the game.
     print("Three doors ahead...")
+    sleep(0.5)
     print("A ghost behind one")
+    sleep(0.5)
     print("Which door do you open?")
     doorChosen = input("1, 2, or 3? ")
     # This is a prompt to the user
@@ -45,12 +59,14 @@ while feelingBrave:
             # Only plays the sound effect if the SFX_enabled variable is True.
         print("You enter the next room.")
         score += 1
+        sleep(0.5)
         # Adds 1 point to the score counter.
 # This code is executed when the user types a number that happens
 # to be the ghost door.
 print("Run away!")
+sleep(0.5)
 print("Game over!\nYou scored", score, "points")
 if SFX_enabled:
     playsound.playsound(LoseSound_filename, True)
-    # Only plays the sound effect 
+    # Only plays the sound effect if the SFX_enabled variable is true.
 input("Press Enter to exit...")
